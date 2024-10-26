af636971d9fc20f6490d38c0cfe7ee1f2302856d
한상희 <tkdgml822@gmail.com>
2024-10-26 22:00:50 +0900

    글작성 - DTO - 컨트롤러

--- src/main/java/com/example/demo/controller/SaveController.java
+++ src/main/java/com/example/demo/controller/SaveController.java
@@ -1,8 +1,17 @@
 package com.example.demo.controller;
 
+import com.example.demo.dto.SaveDTO;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class SaveController {
+
+    @PostMapping("/save")
+    public String saveLogic(SaveDTO saveDTO) {
+        System.out.println(saveDTO);
+
+        return "redirect:/";
+    }
 }
---
+++ src/main/java/com/example/demo/dto/SaveDTO.java
@@ -0,0 +1,14 @@
+package com.example.demo.dto;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+//@Setter
+//@Getter
+@Data
+public class SaveDTO {
+
+    private String title;
+    private String content;
+}
